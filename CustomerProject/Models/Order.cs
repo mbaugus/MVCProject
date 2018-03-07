c874928780a02dceaae2bc36c4b94dd0c6dbf943
Mike Baugus <mbaugus@gmail.com>
2018-03-07 13:47:41 -0500

    New MVC project several addon views

---
+++ CustomerProject/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerProject.Models
+{
+    public class Order
+    {
+        public Order() { }
+        public Order(DateTime date, decimal total, int customerID)
+        {
+            Date = date;
+            Total = total;
+            CustomerId = customerID;
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
